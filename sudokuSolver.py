2f7286cdb3c2356ec998ccaa3a32c261185c72f2
Axob12 <sachithdhanushka00@gmail.com>
2023-01-08 23:14:55 +0530

    Debbuged

--- sudokuSolver.py
+++ sudokuSolver.py
@@ -19,7 +19,7 @@ def valid(bo, num, pos):
 
     # check column
     for i in range(len(bo[0])):
-        if bo[pos[1]][i] == num and pos[0] != i :
+        if bo[i][pos[1]] == num and pos[0] != i :
             return False
 
     # check box
@@ -27,9 +27,19 @@ def valid(bo, num, pos):
     box_y = pos[0] // 3
 
     for i in range(box_y*3, box_y*3 + 3):
-        for i in range(box_x*3, box_x*3 + 3):
+        for j in range(box_x*3, box_x*3 + 3):
             if bo[i][j] == num and (i, j) != pos:
                 return False
+
+    return True
+
+def find_empty(bo):
+
+    for i in range(len(bo)):
+        for j in range(len(bo[0])):
+            if bo[i][j] == 0:
+                return (i, j) #row, col
+    return None
 
 
 def solve(bo):
@@ -68,10 +78,5 @@ def print_board(bo):
                 print(str(bo[i][j]) + " ", end="")
 
 
-def find_empty(bo):
-
-    for i in range(len(bo)):
-        for j in range(len(bo[0])):
-            if bo[i][j] == 0:
-                return (i, j) #row, col
-    return None
+solve(board)
+print_board(board)
